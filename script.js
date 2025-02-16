b7032d2ef8d95042ec8d917e9ac32d19ad1416c7
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-16 15:46:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function mincost(arr)
 { 
+	if(arr.length<=1)
+		return 0;
+	arr.sort((a,b) => a-b);
+
+	let total=0;
+	while(arr.length>1)
+		{
+			const first = arr.shift();
+			const second = arr.shift();
+			const cost = first+second;
+				total = total+const;
+			arr.push(cost);
+			arr.sort((a,b)=>a-b);
+		}
 //write your code here
 // return the min cost
+	return total;
   
 }
 
